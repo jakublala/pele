03c78a9eb250f82f26e4174c5fea5b5d93b504d8
jakublala <jakublala@gmail.com>
2024-07-10 16:27:14 +0200

    fix numpy 2.0

--- setup_with_cmake.py
+++ setup_with_cmake.py
@@ -20,7 +20,7 @@ from numpy.distutils.command.build_ext import build_ext as old_build_ext
 # Numpy header files
 numpy_lib = os.path.split(np.__file__)[0]
 # if numpy > 2.0 then underscore is present
-if os.path.exists(os.path.join(numpy_lib, "_core")):
+if int(np.__version__.split(".")[0]) >= 2:
     numpy_include = os.path.join(numpy_lib, "_core/include")
 else:
     numpy_include = os.path.join(numpy_lib, "core/include")
